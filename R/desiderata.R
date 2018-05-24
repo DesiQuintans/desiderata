ae82ab112eb4570fde8d944673076b4316f1a3f0
DesiQuintans <science@desiquintans.com>
2018-05-24 20:04:23 +1000

    Add package documentation page.

---
+++ R/desiderata.R
@@ -0,0 +1,11 @@
+#' desiderata - A pack of personal functions
+#'
+#' @section Author:
+#' Desi Quintans (@eco_desi)
+#'
+#' @section URL:
+#' \url{https://github.com/DesiQuintans/taxizehelper/}
+#'
+#' @docType package
+#' @name desiderata
+NULL
