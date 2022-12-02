6ac6a2a1e4e774307db7103b04f05ab1b433d827
Desi Quintans <science@desiquintans.com>
2022-12-02 18:33:31 +1100

    Clear out funs that will not be added

--- funs to add.R
+++ funs to add.R
@@ -1,27 +1,4 @@
 
-
-
-
-
-
-
-# Format CIs as 3 numbers truncated to decent length.
-format_ci <- function(str) {
-    ci <- 
-        list(lower = nth_word(str, 1),
-             estim = nth_word(str, 2),
-             upper = nth_word(str, 3))
-    
-    ci <- lapply(ci, stringr::str_pad, width = 5, side = "left", pad = "??")
-    
-    sprintf("\\textsubscript{%s} %s \\textsubscript{%s}", ci$lower, ci$estim, ci$upper)
-}
-
-# Is this useful?
-`%has%` <- function(x, y) {
-    grepl(y, x)
-}
-
 importMultiple <- function(path, ext, func, ...) {
     # Imports multiple files with a particular extension, concatenating them into a single
     # dataframe with a new 'sourceFile' column specifying the origin of each row. Inspired
@@ -35,56 +12,36 @@ importMultiple <- function(path, ext, func, ...) {
     return(bind_rows(listOfDFs, .id = "sourceFile"))
 }
 
-# Collapse multiple strings into one.
-str_coll <- function(v, col = ",") {
-    # If using space or a character as col, can use the regex
-    #   \\b\\w+?\\b 
-    # to grab individual words.
-    paste0(v, collapse = col) %>% str_trim()
-}
 
-```{r voting-algorithm}
-vote_percentages <- function(df) {
-    df %>% 
-        group_by(voter) %>% 
-        filter(rank == min(rank)) %>% 
-        ungroup() %>% 
-        count(bird_breed) %>% 
-        adorn_percentages(denominator = "col") %>% 
-        arrange(n)
-}
-
-a_bird_has_won <- function(result) {
-    any(result$n > 0.50)
-}
-
-drop_least_votes <- function(initial, result) {
-    least_popular <- result[1,]$bird_breed
-    
-    initial %>% 
-        filter(bird_breed != least_popular)
-}
 
 instant_runoff_voting <- function(df) {
+    vote_percentages <- function(df) {
+        df %>% 
+            group_by(voter) %>% 
+            filter(rank == min(rank)) %>% 
+            ungroup() %>% 
+            count(bird_breed) %>% 
+            adorn_percentages(denominator = "col") %>% 
+            arrange(n)
+    }
+    
+    someone_won <- function(result) {
+        any(result$n > 0.50)
+    }
+    
+    drop_least_votes <- function(initial, result) {
+        least_popular <- result[1,]$bird_breed
+        
+        initial %>% 
+            filter(bird_breed != least_popular)
+    }
+    
     vote_result <- vote_percentages(df)
     
-    if (a_bird_has_won(vote_result) == TRUE) {
+    if (someone_won(vote_result) == TRUE) {
         return(vote_result)
     } else {
         drop_least_votes(df, vote_result) %>% 
             instant_runoff_voting()
     }
 }
-```
-
-# Return the first sentence that matches a query near the start of a sentence.
-subsentence <- function(str, query, buffer = 10) {
-    keywords <- ifelse(length(query) == 1,
-                       query,
-                       paste0("(", paste(query, collapse = "|"), ")"))
-    needle   <- paste0("(\\. |; |^).{0,", buffer, "}", keywords, ".*?(\\. [a-z]|$)")
-    
-    str_extract(str, regex(needle, ignore_case = TRUE))
-}
-
-
